ad08e351723ddb66790f3f443e82ec18116f98f2
prakashranjan <prakashranjansingh04@gmail.com>
2020-11-26 20:42:44 +0530

    test2

--- pySeleniumCode/test2.py
+++ pySeleniumCode/test2.py
@@ -1,7 +1,11 @@
 from selenium import webdriver
 
+
 browser = webdriver.Chrome()
-browser.get("https://www.google.com")
+browser.set_window_size(1024, 768)
+
+browser.get("https://www.moneycontrol.com/")
+
 
 
 browser.close()
